6b507c9734e902feba6cf1b815808deac31d613b
John Nguyen <nguyen.john@me.com>
2012-03-24 13:24:56 -0400

    first commit

---
+++ src/mobile/team/reservations/PanicLightActivity.java
@@ -0,0 +1,13 @@
+package mobile.team.reservations;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PanicLightActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
